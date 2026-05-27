155ac2af087f928690c40706c0ee8c13af786568
Mykyta <mykytakhmyliuk@gmail.com>
2026-05-27 21:04:43 +0300

    feat(network): add cloud firewall with minecraft and icmp rules

---
+++ infra/terraform/firewall.tf
@@ -0,0 +1,26 @@
+resource "hcloud_firewall" "my-firewall" {
+  name = "my-firewall"
+
+  rule {
+    direction  = "in"
+    protocol   = "icmp"
+    source_ips = ["0.0.0.0/0", "::/0"]
+  }
+
+  rule {
+    direction  = "in"
+    protocol   = "tcp"
+    port       = "25565"
+    source_ips = ["0.0.0.0/0", "::/0"]
+    description = "Minecraft java server"
+  }
+
+  rule {
+    direction  = "in"
+    protocol   = "tcp"
+    port       = "25566"
+    source_ips = ["0.0.0.0/0", "::/0"]
+    description = "BlueMap"
+  }
+
+}
